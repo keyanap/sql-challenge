ac9bc451868eafba7b73c4fee9682ec7da2f7390
keyanap <keyanapregent@gmail.com>
2023-10-18 09:41:24 -0400

    data engineering

---
+++ EmployeeSQL/employee_schema.sql
@@ -0,0 +1,34 @@
+-- create titles table
+CREATE TABLE titles (
+	title_id VARCHAR (5) PRIMARY KEY,
+	title VARCHAR (30));
+
+-- create employees table
+CREATE TABLE employees (
+	emp_no VARCHAR (10) PRIMARY KEY,
+	emp_title_id VARCHAR (5) references titles(title_id),
+	birth_date DATE,
+	first_name VARCHAR (30),
+	last_nast VARCHAR (30),
+	sex VARCHAR (1),
+	hire_date DATE);
+
+-- create departments table
+CREATE TABLE departments (
+	dept_no VARCHAR (5) PRIMARY KEY,
+	dept_name VARCHAR (30));
+
+-- create dept_emp table
+CREATE TABLE dept_emp (
+	emp_no VARCHAR (10) references employees(emp_no),
+	dept_no VARCHAR (5) references departments(dept_no));
+	
+-- create dept_manager table
+CREATE TABLE dept_manager (
+	dept_no VARCHAR (5) references departments(dept_no),
+	emp_no VARCHAR (10) references employees(emp_no));
+	
+-- create salaries table
+CREATE TABLE salaries (
+	emp_no VARCHAR (10) references employees(emp_no),
+	salary INT);
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Data Modelling 
 ![Alt text](<QuickDBD-Free Diagram.png>)
 
+Data Engineering
+![Alt text](<Screenshot 2023-10-18 at 9.39.40 AM.png>)
+
+
+Data Analysis
